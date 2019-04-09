62257279580ddf9dfd32b0d70336fd5accf15512
Blake Stoner <stonerblake@gmail.com>
2019-04-09 20:46:33 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,3 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-
-def greeting 
-  
-  greeting = "Hello World"
+ # Define a local variable called 'greetin
+ 
+ greeting = "Hello World"
